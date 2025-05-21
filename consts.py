162476f842a750e8dad696ffb9d929a94a3e44fb
RichardTry <bazrovstepan@gmail.com>
2025-05-21 11:46:03 +0300

    Add constants

---
+++ consts.py
@@ -0,0 +1,7 @@
+# Константы
+WIDTH, HEIGHT = 1920, 980
+TILE_SIZE = 40
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+PLAYER_COLOR = (0, 255, 0)  # Зеленый цвет для игрока
+BACKGROUND_COLOR = (0, 0, 0)  # Черный цвет фона
